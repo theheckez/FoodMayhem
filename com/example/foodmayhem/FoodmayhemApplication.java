a550e12e60a446e30b17ee3cccc0ca9ed2dae850
Popeyes <79601036+theheckez@users.noreply.github.com>
2022-12-21 01:04:58 +0100

    JAVA API REST FILES

---
+++ com/example/foodmayhem/FoodmayhemApplication.java
@@ -0,0 +1,18 @@
+package com.example.foodmayhem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
+
+
+
+
+@SpringBootApplication
+
+
+public class FoodmayhemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmayhemApplication.class, args);
+	}
+
+}
