94599d63a16faa87dd5abdcdc162beee3e087d7f
Popeyes <dragonier2013@gmail.com>
2023-06-30 23:50:41 +0200

    Añadidos archivos que faltaban

---
+++ FoodMayhem/foodmayhem/src/main/resources/static/js/WebSocketFiles/MovementWebSocket.js
@@ -0,0 +1,54 @@
+movementWebSocket.onopen = function () 
+{
+	console.log("Websocket de movimiento activado");
+}
+
+movementWebSocket.onerror = function (e) {
+    console.log("WS error: " + e);
+}
+
+movementWebSocket.onmessage = function (msg) {
+	var obj = JSON.parse(msg.data);
+	if (obj.player_id == 1)
+	{
+		player1.x = obj.player_x;
+		player1.y = obj.player_y;
+	  
+		player1.padMovement = obj.player_pad;
+		
+		player1.dirUp = obj.player_dirUp;
+		player1.dirLeft = obj.player_dirLeft;
+		player1.dirRight = obj.player_dirRight;
+		player1.dirDown = obj.player_dirDown;
+	}
+	
+	if(obj.player_id == 2 ){
+		player2.x = obj.player_x;
+		player2.y = obj.player_y;
+	  
+		player2.padMovement = obj.player_pad;
+		
+		player2.dirUp = obj.player_dirUp;
+		player2.dirLeft = obj.player_dirLeft;
+		player2.dirRight = obj.player_dirRight;
+		player2.dirDown = obj.player_dirDown;
+	}
+	console.log("Recibo movimientos");
+}
+
+movementWebSocket.sendWS = function (id, x, y, pad, up, left, down, right) {
+	let message = {
+            player_id: id,
+			player_x: x,
+			player_y: y,
+            player_pad: pad,
+            player_dirUp : up,
+            player_dirLeft : left,
+            player_dirDown : down,
+            player_dirRight : right,			
+	};
+	
+	var mes = JSON.stringify(message)
+		
+	movementWebSocket.send(mes);
+}
---
+++ FoodMayhem/foodmayhem/src/main/resources/static/js/WebSocketFiles/WebSocketVariables.js
@@ -0,0 +1,17 @@
+//<summary>
+// Declaration of WEBSOCKET variables to use in teh rest of .js files
+// All (____)Path variables indicate the path where the WebSocket should be listening.
+// All (____)WebSocket variables are references to WebSockets created with the corresponding
+//              path assigned.
+//</summary>
+
+var movementPath = 'ws://127.0.0.1:8080/movement';
+var movementWebSocket = new WebSocket(movementPath);
+
+var connectionPath = 'ws://127.0.0.1:8080/sessions';
+var connectionWebSocket;
+
+
+var generationPath = 'ws://127.0.0.1:8080/randomGeneration';
+var generationWebSocket = new WebSocket(generationPath);
+
